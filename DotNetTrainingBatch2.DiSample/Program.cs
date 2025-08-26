6553b8dbef437c3b120a82f23ccc79500adbc517
WaiZin <WaiZin@DESKTOP-PC92E8F>
2025-08-26 16:51:29 +0630

    feat: di sample

--- DotNetTrainingBatch2.DiSample/Program.cs
+++ DotNetTrainingBatch2.DiSample/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 builder.Services.AddScoped<ProductService>();
 builder.Services.AddScoped<ProductDataAccess>();
 builder.Services.AddControllers();
+//hello
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,6 +22,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
+
 {
     app.UseSwagger();
     app.UseSwaggerUI();
